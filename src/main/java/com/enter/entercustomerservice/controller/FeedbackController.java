308e1415b3c987008fc1d6dc72a60559c7c9d13a
liquidRWQ <838860376@qq.com>
2019-10-02 20:41:47 +0800

    模块1.0完成

--- src/main/java/com/enter/entercustomerservice/controller/FeedbackController.java
+++ src/main/java/com/enter/entercustomerservice/controller/FeedbackController.java
@@ -7,33 +7,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Liquid
+ * @version 1.0
+ * @date 2019/10/2
+ */
+
 @RestController
 public class FeedbackController {
     @Autowired
     private FeedbackService feedbackService;
 
     @RequestMapping("/insertFeedback")
-    public void insertFeebdack(FeedbackDTO feedbackDTO) throws Exception {
+    public void insertFeedback(FeedbackDTO feedbackDTO) throws Exception {
         feedbackService.insert(feedbackDTO);
     }
 
     @RequestMapping("/selectAllFeedback")
-    public ResultBean selectAllFeeback(){
-        return new ResultBean(feedbackService.selectAll());
+    public ResultBean selectAllFeedback() {
+        return new ResultBean<>(feedbackService.selectAll());
     }
 
     @RequestMapping("/selectByFeedbackPrimaryKey")
-    public ResultBean selectByFeedbackPrimaryKey(FeedbackDTO feedbackDTO){
-        return new ResultBean(feedbackService.selectByPrimary(feedbackDTO));
+    public ResultBean selectByFeedbackPrimaryKey(FeedbackDTO feedbackDTO) {
+        return new ResultBean<>(feedbackService.selectByPrimary(feedbackDTO));
     }
 
     @RequestMapping("/updateFeedback")
-    public void updateFeedback(FeedbackDTO feedbackDTO){
+    public void updateFeedback(FeedbackDTO feedbackDTO) {
         feedbackService.updateByPrimary(feedbackDTO);
     }
 
     @RequestMapping("/deleteFeedbackByPrimaryKey")
-    public void deleteFeedback(FeedbackDTO feedbackDTO){
+    public void deleteFeedback(FeedbackDTO feedbackDTO) {
         feedbackService.deleteByPrimary(feedbackDTO);
     }
 }
